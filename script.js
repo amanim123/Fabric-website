681b37381215e9916ceb9e3f2638b1550d32d25e
amanim123 <127251129+amanim123@users.noreply.github.com>
2023-03-07 15:44:36 -0500

    Fabric website first draft

---
+++ script.js
@@ -0,0 +1,100 @@
+// $('.img1').hover(() => {switchOn('.img1')}, () => {switchOff('.img1')})
+// $('.img2').hover(() => {switchOn('.img2')}, () => {switchOff('.img2')})
+// $('.img3').hover(() => {switchbig('.img3')}, () => {switchsmall('.img3')})
+
+
+// function switchOn(name){
+//     $(name).css("opacity", "1")
+// }
+// function switchOff(name){
+//     $(name).css("opacity", "0")
+// }
+// function switchbig(name){
+//     $(name).css("opacity", "1")
+// }
+// function switchsmall(name){
+//     $(name).css("opacity", "0")
+// }
+
+
+// $(".ep1").on("click", () => {
+//       if ($(".ep1").css("width") == "400px") {
+//         $(".ep1").css("width", "100px");
+//         $(".ep1").css("height", "100px");
+//       } else {
+//         console.log($(".ep1").css("width"));
+//         $(".ep1").css("width", "400px");
+//         $(".ep1").css("height", "400px");
+//       }
+//     });
+
+//   $(".ep2").on("click", () => {
+//           if ($(".ep2").css("width") == "400px") {
+//             $(".ep2").css("width", "100px");
+//             $(".ep2").css("height", "100px");
+//           } else {
+//             console.log($(".ep2").css("width"));
+//             $(".ep2").css("width", "400px");
+//             $(".ep2").css("height", "400px");
+//           }
+//         });
+var clickableArea = document.getElementById("clickable-area");
+
+clickableArea.addEventListener("click", function() {
+window.location.href = "cotton.html";
+});
+
+var clickableArea = document.getElementById("clickable-area1");
+clickableArea.addEventListener("click", function() {
+window.location.href = "Linen.html";
+});
+
+var clickableArea = document.getElementById("clickable-area2");
+clickableArea.addEventListener("click", function() {
+window.location.href = "Wool.html";
+});
+
+var clickableArea = document.getElementById("clickable-area3");
+clickableArea.addEventListener("click", function() {
+window.location.href = "Silk.html";
+});
+
+var clickableArea = document.getElementById("clickable-area4");
+clickableArea.addEventListener("click", function() {
+window.location.href = "Polyester.html";
+});
+
+var clickableArea = document.getElementById("clickable-area5");
+clickableArea.addEventListener("click", function() {
+window.location.href = "Satin.html";
+});
+
+var clickableArea = document.getElementById("clickable-area6");
+clickableArea.addEventListener("click", function() {
+window.location.href = "Leather.html";
+});
+
+var clickableArea = document.getElementById("clickable-area7");
+clickableArea.addEventListener("click", function() {
+window.location.href = "Hemp.html";
+});
+
+var clickableArea = document.getElementById("clickable-area8");
+clickableArea.addEventListener("click", function() {
+window.location.href = "Bamboo.html";
+});
+
+var clickableArea = document.getElementById("clickable-area9");
+clickableArea.addEventListener("click", function() {
+window.location.href = "Lace.html";
+});
+
+var clickableArea = document.getElementById("clickable-area10");
+clickableArea.addEventListener("click", function() {
+window.location.href = "Spandex.html";
+});
+
+var clickableArea = document.getElementById("clickable-area11");
+clickableArea.addEventListener("click", function() {
+window.location.href = "Velvet.html";
+});
